9d9640cde55fde2281031216908f37bd7c0976f3
Augusto Vasconcelos <augustovnd@gmail.com>
2021-08-07 00:58:28 -0300

    *commit

--- src/main/java/Conta.java
+++ src/main/java/Conta.java
@@ -4,40 +4,92 @@
  */
 public class Conta {
 
+    private int num, op =0;
+
+    private double sal;
+
+    private double limite = 100;
+
+    private double[] extrato = new double[10];
+
     public Conta(int numero, double saldo) {
+        num=numero;
+        sal=saldo;
     }
 
     
     public int getNumero() {
-        return 0;
+        return num;
     }
     
     public double getSaldo() {
-        return 0;
+        return sal+limite;
     }
 
     
     public double getLimite() {
-        return 0;
+        return limite;
     }
     
     public boolean sacar(double valor) {
-        return false;
+        if(valor<0)
+            return false;
+        else if(valor>sal+limite)
+            return false;
+        else {
+            extrato[op]=valor*-1;
+            op++;
+            if(valor>sal) {
+                valor-=sal;
+                sal=0;
+                limite-=valor;
+            }
+            else
+                sal-=valor;
+            return true;
+        }
     }
 
    
     public boolean depositar(double valor) {
-        return true;
+        if(valor<0)
+            return false;
+        else {
+            extrato[op]=valor;
+            op++;
+            if(limite<100) {
+                sal+=Math.max(0,valor-100+limite);
+                limite+=Math.min(valor,100-limite);
+            }
+            else
+                sal+=valor;
+            return true;
+        }
     }
 
     
     public boolean transferir(Conta destino, double valor) {
-        return false;
+        if(valor<0)
+            return false;
+        else if(valor>sal+limite)
+            return false;
+        else {
+            extrato[op]=valor*-1;
+            op++;
+            destino.depositar(valor);
+            if(valor>sal) {
+                valor-=sal;
+                sal=0;
+                limite-=valor;
+            }
+            else
+                sal-=valor;
+            return true;
+        }
     }
 
 
     public double[] verExtrato() {
-
-        return null;
+        return extrato;
     }
 }
